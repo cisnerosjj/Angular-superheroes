135aa1e6f06642718e3740c95e65baf4c0c7e442
cisnerosjj <jorgecisnerosgavin@gmail.com>
2024-05-12 22:19:38 +0200

    Finish Search feature :sparkles:

--- src/app/components/search-superheroes/search-superheroes.component.ts
+++ src/app/components/search-superheroes/search-superheroes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { SuperheroService } from 'src/app/services/superhero.service';
 import { Superhero } from 'src/superhero.model';
 
@@ -8,14 +10,28 @@ import { Superhero } from 'src/superhero.model';
   styleUrls: ['./search-superheroes.component.css'],
 })
 export class SearchSuperheroesComponent {
+  public searchInput = new FormControl('');
   public superheroes: Superhero[] = [];
+  public selectedSuperhero?: Superhero;
 
   constructor(private superheroService: SuperheroService) {}
 
-  ngOnInit(): void {
+  searchSuperhero() {
+    const inputValue: string = this.searchInput.value || '';
+
     this.superheroService
-      .getSuperheroes()
+      .getSuggestions(inputValue)
       .subscribe((superheroes) => (this.superheroes = superheroes));
-    console.log('llego');
+  }
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value) {
+      this.selectedSuperhero = undefined;
+      return;
+    }
+
+    const superhero: Superhero = event.option.value;
+    this.searchInput.setValue(superhero.superhero);
+
+    this.selectedSuperhero = superhero;
   }
 }
